44af7e62c65284f81da899a5cde990e28029019e
Andrew Swerlick <andrew.swerlick@hbdi.com>
2017-02-06 14:41:23 -0500

    adding postgres 5 support

--- lib/schema_friendly/structure_dump_patch.rb
+++ lib/schema_friendly/structure_dump_patch.rb
@@ -2,7 +2,7 @@ ActiveRecord::Tasks::PostgreSQLDatabaseTasks.class_eval do
   def structure_dump(filename)
     set_psql_env
 
-    command = "pg_dump -i -s -x -O -f #{Shellwords.escape(filename)} #{Shellwords.escape(configuration['database'])}"
+    command = "pg_dump -s -x -O -f #{Shellwords.escape(filename)} #{Shellwords.escape(configuration['database'])}"
     raise 'Error dumping database' unless Kernel.system(command)
 
     File.open(filename, "a") { |f| f << "SET search_path TO #{connection.schema_search_path};\n\n" }
