c64f41551abb1a19d88b0d0f714fa4310620cc0e
Andrew Swerlick <andrew.swerlick@hbdi.com>
2017-04-26 11:03:51 -0400

    removing unecessary patching for rails 5.0 and above

--- lib/schema_friendly/structure_dump_patch.rb
+++ lib/schema_friendly/structure_dump_patch.rb
@@ -1,10 +1,12 @@
-ActiveRecord::Tasks::PostgreSQLDatabaseTasks.class_eval do
-  def structure_dump(filename)
-    set_psql_env
+if defined(Rails) && Rails::VERSION::MAJOR < 5
+  ActiveRecord::Tasks::PostgreSQLDatabaseTasks.class_eval do
+    def structure_dump(filename)
+      set_psql_env
 
-    command = "pg_dump -s -x -O -f #{Shellwords.escape(filename)} #{Shellwords.escape(configuration['database'])}"
-    raise 'Error dumping database' unless Kernel.system(command)
+      command = "pg_dump -s -x -O -f #{Shellwords.escape(filename)} #{Shellwords.escape(configuration['database'])}"
+      raise 'Error dumping database' unless Kernel.system(command)
 
-    File.open(filename, "a") { |f| f << "SET search_path TO #{connection.schema_search_path};\n\n" }
+      File.open(filename, "a") { |f| f << "SET search_path TO #{connection.schema_search_path};\n\n" }
+    end
   end
 end
