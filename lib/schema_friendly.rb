5118f076c4e808a4f5c0cdbf11da89784b0eef6e
Andrew Swerlick <andrew.swerlick@hbdi.com>
2017-04-27 15:48:40 -0400

    trying to solve rails test error

--- lib/schema_friendly.rb
+++ lib/schema_friendly.rb
@@ -1,7 +1,5 @@
 require "schema_friendly/version"
 require "active_record"
-require 'schema_friendly/structure_dump_patch'
-require 'schema_friendly/migration_patches'
 require 'schema_friendly/railtie' if defined?(Rails)
 
 module SchemaFriendly
--- lib/schema_friendly/railtie.rb
+++ lib/schema_friendly/railtie.rb
@@ -3,6 +3,8 @@ module SchemaFriendly
     railtie_name :schema_friendly
 
     rake_tasks do
+      require 'schema_friendly/structure_dump_patch'
+      require 'schema_friendly/structure_dump_patch'
       load "schema_friendly/tasks.rake"
     end
   end
